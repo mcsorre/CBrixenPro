d3f5c1e255e9e3d2a70cb5407aa83e5d10e5b818
mcsorre <157872838+mcsorre@users.noreply.github.com>
2024-01-27 08:36:27 +0100

    Update example.py

--- example.py
+++ example.py
@@ -4,26 +4,28 @@ from PIL import Image
 from gtts import gTTS
 from translate import Translator
 
-if "user_input" not in st.session_state:
-    st.session_state.user_input = None
-if "rand_item" not in st.session_state:
-    st.session_state.rand_item = None
 
-# Define functions
+if "page" not in st.session_state:
+  st.session_state.page = 0
+if "user_input" not in st.session_state:
+  st.session_state.user_input = None
+if "rand_item" not in st.session_state:
+  st.session_state.rand_item = None
+
 def restart():
-    st.session_state.rand_item = random.choice(['chair', 'cold', 'earth', 'friend', 'heart', 'mountain', 'mouse', 'snow', 'woman'])
+  st.session_state.page = 0
+  st.session_state.rand_item = random.choice(['chair', 'cold', 'earth', 'friend', 'heart', 'mountain', 'mouse', 'snow', 'woman'])
 
 placeholder = st.empty()
 
-items = ['chair', 'cold', 'earth', 'friend', 'heart', 'mountain', 'mouse', 'snow', 'woman']
-
-if 'rand_item' not in st.session_state:
+if st.session_state.page == 0:
+  items = ['chair', 'cold', 'earth', 'friend', 'heart', 'mountain', 'mouse', 'snow', 'woman']   
+  if 'item' not in st.session_state:
     st.session_state.rand_item = random.choice(items)
-
-with st.sidebar:
-    st.write("")
-
-with placeholder.container():
+    st.session_state.item = st.session_state.rand_item
+  else:
+    rand_item = st.session_state.item
+  with placeholder.container():
     st.title("The Ultimate IPA Quiz: Decoding English Words")
     st.write("\n")
     st.text("Embark on a linguistic adventure with the immersive IPA (International Phonetic Alphabet) Quiz, designed to challenge and enhance your understanding of English pronunciation")
@@ -31,35 +33,40 @@ with placeholder.container():
     st.header("Decipher the IPA symbol :uk: :uk: :uk:")
     st.write("\n")
     st.write("\n")
+  picture = "image/" + st.session_state.rand_item + '.jpg'
+  img = Image.open(picture)
+  st.image(img, width=300)
+  st.session_state.user_input = st.text_input("Type in the English word you see in the IPA symbol", key=1)
+  st.button("Continue",on_click=restart,disabled=(st.session_state.page = 0))
+  
 
-picture = "image/" + st.session_state.rand_item + '.jpg'
-img = Image.open(picture)
+elif st.session_state.page = 0:
+  if st.session_state.user_input:
+    if st.session_state.user_input.lower() == str(st.session_state.rand_item):
+      text_to_translate = str(st.session_state.rand_item)
+      translator = Translator(to_lang='it')
+      result = Translator.translate(translator, text_to_translate)
+      picture = "image/" + st.session_state.rand_item + '.jpg'
+      img = Image.open(picture)
+      st.image(img, width=300)
+      placeholder.header("Great job! That is correct!.")
+      st.write("The word '",st.session_state.rand_item,"' translates to '",result,"' in italian.")
+      st.write("Now you can practice the pronunciation of this word")
+      tts=gTTS(text= st.session_state.rand_item, lang='en')
+      tts.save('user.mp3')
+      st.audio('user.mp3')
 
-with st.form(key="answer_form"):
-    st.image(img, width=300)
-    st.session_state.user_input = st.text_input("Type in the English word you see in the IPA symbol", key=1)
-    submit_button = st.form_submit_button("Submit")
-
-if submit_button:
-    if st.session_state.user_input:
-        if st.session_state.user_input.lower() == str(st.session_state.rand_item):
-            text_to_translate = str(st.session_state.rand_item)
-            translator = Translator(to_lang='it')
-            result = Translator.translate(translator, text_to_translate)
-            placeholder.header("Great job! That is correct!.")
-            st.write("The word '", st.session_state.rand_item, "' translates to '", result, "' in Italian.")
-            st.write("Now you can practice the pronunciation of this word")
-            tts = gTTS(text=st.session_state.rand_item, lang='en')
-            tts.save('user.mp3')
-            st.audio('user.mp3')
-        else:
-            text_to_translate = str(st.session_state.rand_item)
-            translator = Translator(to_lang='it')
-            result = Translator.translate(translator, text_to_translate)
-            placeholder.header("Unfortunately, that is not correct.")
-            st.write("The correct word was '", st.session_state.rand_item, "'.")
-            st.write("The word '", st.session_state.rand_item, "' translates to '", result, "' in Italian.")
-            st.write("Now you can practice the pronunciation of this word")
-            tts = gTTS(text=st.session_state.rand_item, lang='en')
-            tts.save('user.mp3')
-            st.audio('user.mp3')
+    else:      
+      text_to_translate = str(st.session_state.rand_item)
+      translator = Translator(to_lang='it')
+      result = Translator.translate(translator, text_to_translate)
+      picture = "image/" + st.session_state.rand_item + '.jpg'
+      img = Image.open(picture)
+      st.image(img, width=300)
+      placeholder.header("Unfortunately, that is not correct.")
+      st.write("The correct word was '",st.session_state.rand_item,"'.")
+      st.write("The word '",st.session_state.rand_item,"' translates to '",result,"' in italian.")
+      st.write("Now you can practice the pronunciation of this word")
+      tts=gTTS(text= st.session_state.rand_item, lang='en')
+      tts.save('user.mp3')
+      st.audio('user.mp3')
