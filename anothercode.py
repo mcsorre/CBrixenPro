bc251a0a9c6cea4b45ca10a68625d5aad889bf29
mcsorre <157872838+mcsorre@users.noreply.github.com>
2024-01-27 10:13:28 +0100

    Update anothercode.py

--- anothercode.py
+++ anothercode.py
@@ -49,7 +49,7 @@ if st.session_state.page == 0:
   picture = "image/" + st.session_state.rand_item + '.jpg'
   img = Image.open(picture)
   st.image(img, width=300)
-  st.session_state.user_input = st.text_input("Type in the English word you see in the IPA symbol. Remember to first press _Enter_ and then click on _Continue_", key=1)
+  st.session_state.user_input = st.text_input("Type in the English word you see in the IPA symbol. **Remember to first press Enter and then click on Continue**", key=1)
   st.button("Continue",on_click=nextpage,disabled=(st.session_state.page > 1))
   
 
